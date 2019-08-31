cd9abde06e196c90bf475188df83ece86c0310ab
mpalad.rails <happyfighter2015@gmail.com>
2019-08-31 09:17:47 -0400

    Creation of screenshot

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, request
+from redis import Redis
+from rq import Queue
 from tasks import create_url_screenshot
 
 app = Flask(__name__)
-
+redis = Redis()
+q = Queue(connection=redis)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        name = request.form.get('name')
         url = request.form.get('url')
-        return 'You entered {}'.format(url)
+        
+        q.enqueue(create_url_screenshot, name, url)
+        return 'Screenshot being taken for {} ({})'.format(name, url)
     return render_template('index.html')
 
 
