5eb3f62cc2a52fd65a752d2b0c380e3b31108957
mpalad.rails <happyfighter2015@gmail.com>
2019-08-31 09:05:47 -0400

    Ad tasks and templates

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from tasks import create_url_screenshot
 
 app = Flask(__name__)
 
-@app.route('/')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Webshot</h1>'
+    if request.method == 'POST':
+        url = request.form.get('url')
+        return 'You entered {}'.format(url)
+    return render_template('index.html')
+
+
 
 
---
+++ tasks.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+
+def create_url_screenshot(name, url):
+    print('Create screenshot for {}'.format(url))
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
+    options.add_argument("--headless")
+    driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=options)  
+    #driver.set_window_size(1920, 1080, driver.window_handles[0])
+    driver.set_window_size(1440, 900, driver.window_handles[0])
+
+    driver.get(url)
+    filename = '{}.png'.forma(name.lower())
+    driver.save_screenshot(filename)
+    driver.close()
+    
+    print('Screenshot saves as {}'.format(filename))
