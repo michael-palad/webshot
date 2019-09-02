6488d1d41ab2ede3782ee92bd3225f0e428f9e53
mpalad.rails <happyfighter2015@gmail.com>
2019-09-02 13:51:20 -0400

    Change message page to redirect to support the PRG pattern

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import glob
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from redis import Redis
 from rq import Queue
 from tasks import create_url_screenshot
@@ -17,15 +17,20 @@ def index():
         url = request.form.get('url')
         
         q.enqueue(create_url_screenshot, name, url)
-        return render_template('message.html', name=name, url=url)
+        return redirect(url_for('message', name=name, url=url))
     
     return render_template('index.html')
 
 
+@app.route('/message')
+def message():
+    name = request.args.get('name')
+    url = request.args.get('url')
+    return render_template('message.html', name=name, url=url)
+
+
 @app.route('/screenshots')
 def screenshots():
-    #screencaps_dir = os.path.join(os.path.dirname(app.instance_path), 'static/screenshots')
-    #image_files = os.listdir(screencaps_dir)
     image_files = [image_file for image_file in glob.glob('static/screenshots/*.png') if not image_file.endswith('_tn.png')]
     image_files.sort(key=os.path.getmtime, reverse=True)
     image_names = [image_name.split('.png')[0].rsplit('/', 2)[-1] for image_name in image_files]
